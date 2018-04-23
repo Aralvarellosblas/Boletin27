6b5cc8905c818b2c5c2f19900c7a535596284a65
arturo <arturo@ArturoMSI>
2018-04-23 09:04:50 +0200

    Inicio Proyecto

---
+++ Ejercicio27/src/ejercicio27/Ejercicio27.java
@@ -0,0 +1,16 @@
+package ejercicio27;
+
+/**
+ *
+ * @author Arturo
+ */
+public class Ejercicio27 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
