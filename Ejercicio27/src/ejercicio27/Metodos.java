45523b50c0570d6677c7506dbdd87f391cdf8397
arturo <arturo@ArturoMSI>
2018-04-24 09:38:11 +0200

    Calculadora finalizada

--- Ejercicio27/src/ejercicio27/Metodos.java
+++ Ejercicio27/src/ejercicio27/Metodos.java
@@ -1,9 +1,18 @@
 package ejercicio27;
 
+import static ejercicio27.Calculadora.cad;
+
 /**
  *
  * @author Arturo
  */
 public class Metodos {
+    public static String arrayString(){
+        String num="";
+        for(int i=0; i<cad.size(); i++){
+            num=num+cad.get(i);
+        }
+        return num;
+    }
     
 }
