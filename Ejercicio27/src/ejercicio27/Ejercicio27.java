f0ae3b3cdcf0788309ee1225d39d071066166491
arturo <arturo@ArturoMSI>
2018-04-24 09:38:04 +0200

    Calculadora finalizada

--- Ejercicio27/src/ejercicio27/Ejercicio27.java
+++ Ejercicio27/src/ejercicio27/Ejercicio27.java
@@ -10,7 +10,7 @@ public class Ejercicio27 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        Calculadora calc =new Calculadora();
     }
 
 }
